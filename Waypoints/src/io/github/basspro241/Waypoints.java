0c8122ab09eae33ed2e8f3ae6bed89f327288be9
BassPro241 <crashal96@aol.com>
2014-02-08 17:52:59 -0500

    Stuff.

--- Waypoints/src/io/github/basspro241/Waypoints.java
+++ Waypoints/src/io/github/basspro241/Waypoints.java
@@ -1,5 +1,13 @@
 package io.github.basspro241;
 
-public class Waypoints {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Waypoints extends JavaPlugin {
+	public void onEnable(){
+		
+	}
+ 
+	public void onDisable(){
+		
+	}
 }

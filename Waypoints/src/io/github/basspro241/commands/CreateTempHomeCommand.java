268f52de4ddea0565d73367455f97791ef09ee0a
BassPro241 <crashal96@aol.com>
2014-02-13 13:53:48 -0500

    Stuffs

---
+++ Waypoints/src/io/github/basspro241/commands/CreateTempHomeCommand.java
@@ -0,0 +1,25 @@
+package io.github.basspro241.commands;
+
+import io.github.basspro241.Waypoints;
+
+import org.bukkit.command.CommandSender;
+
+public class CreateTempHomeCommand extends WaypointCommand implements Command{
+
+	private Waypoints plugin;
+
+	public CreateTempHomeCommand(String name) {
+		super("SetHome");
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean execute(CommandSender executor, String identifier,
+			String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
